125f238fb91c67fc4e76eda39fec528f9ee2948a
Schatzer ZD <daniel.walaker@gmail.com>
2022-11-04 10:54:50 +0100

    Testing string to bytes

--- src/Compression.java
+++ src/Compression.java
@@ -1,6 +1,22 @@
+import java.nio.charset.StandardCharsets;
+
 public class Compression {
 
-    
+
+
+
+
+    public static void main(String[] args) {
+        String test = "JegheterDaniel";
+        System.out.println(test);
+        byte[] textToBytes = test.getBytes(StandardCharsets.UTF_8);
+        System.out.println(textToBytes.length);
+        System.out.println();
+
+        for (byte b: textToBytes) {
+            System.out.print(b + " ");
+        }
+    }
 
 
 }
